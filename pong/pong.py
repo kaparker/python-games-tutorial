0be20cf6ebe208ca34710ad128c85da5bc8cb977
Kerry Parker <kerry.parker@iqblade.com>
2019-11-11 09:01:16 +0000

    Part 2: Game Objects

--- pong/pong.py
+++ pong/pong.py
@@ -1,6 +1,6 @@
 # Simple Pong in Python 3 for beginners
 # Course by @TokyoEdTech
-# Part 1: Getting Started
+# Part 2: Game Objects
 
 import turtle
 
@@ -9,6 +9,32 @@ wn.title("Pong By @TokyoEdTech")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
 
 # Main game loop
 while True:
