1a7307a82562679123bc0f0d770cc8bdbcd1adbe
Kerry Parker <kerry.parker@iqblade.com>
2019-11-18 08:38:44 +0000

    Connectfour: check valid move, add drop piece function

--- connectfour/connectfour.py
+++ connectfour/connectfour.py
@@ -7,8 +7,8 @@ def create_board():
     board = np.zeros((ROW_COUNT,COLUMN_COUNT))
     return board
 
-def drop_piece():
-    pass
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
 
 def is_valid_location(board, col):
     return board[5][col] == 0
@@ -26,11 +26,18 @@ turn = 0
 while not game_over:
     # Ask for Player 1 input
     if turn == 0:
-        selection = int(input("Player 1 Make your selection (0-6)"))
+        col = int(input("Player 1 Make your selection (0-6)"))
 
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
     # Ask for Player 2 input
     else:
-        selection = int(input("Player 2 Make your selection (0-6)"))
+        col = int(input("Player 2 Make your selection (0-6)"))
     
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
+    print(board)
     turn += 1
     turn = turn % 2
