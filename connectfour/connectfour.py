beefb363f05a1e6806a35c65494fb5eade63a51f
Kerry Parker <kerry.parker@iqblade.com>
2019-11-18 08:26:28 +0000

    Connectfour: add player 2 input

--- connectfour/connectfour.py
+++ connectfour/connectfour.py
@@ -5,4 +5,15 @@ def create_board():
     return board
 
 board = create_board()
-print(board)
+game_over = False
+turn = 0
+
+while not game_over:
+    # Ask for Player 1 input
+    if turn == 0:
+        selection = int(input("Player 1 Make your selection (0-6)"))
+
+    # Ask for Player 2 input
+    else:
+        selection = int(input("Player 1 Make your selection (0-6)"))
+        
