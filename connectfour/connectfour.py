1b88a4ef8e4376c777f1e8b1e297ce3abf3da5ff
Kerry Parker <kerry.parker@iqblade.com>
2019-11-19 08:26:50 +0000

    Connectfour: check for wins

--- connectfour/connectfour.py
+++ connectfour/connectfour.py
@@ -11,7 +11,7 @@ def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
@@ -21,6 +21,30 @@ def get_next_open_row(board, col):
 def print_board(board):
     print(np.flip(board, 0))
 
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+    
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+    
+    # Check for positive diagonal
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True    
+
+    # Check for negative diagonal
+    for c in range(COLUMN_COUNT):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True  
 board = create_board()
 game_over = False
 turn = 0
@@ -33,13 +57,22 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
+
+            if winning_move(board, 1):
+                print('Player 1 wins!')
+                game_over = True
     # Ask for Player 2 input
     else:
         col = int(input("Player 2 Make your selection (0-6)"))
     
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
-            drop_piece(board, row, col, 1)
+            drop_piece(board, row, col, 2)
+
+            if winning_move(board, 1):
+                print('Player 2 wins!')
+                game_over = True
+                
     print_board(board)
     turn += 1
     turn = turn % 2
